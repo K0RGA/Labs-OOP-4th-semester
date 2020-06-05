e248f5808aaf5302245cbe4865fa0649d1b23e77
Nikita <nikita-gon1@mail.ru>
2020-06-05 04:30:43 +0300

    crete rep

---
+++ Seminar4/Seminar4/Seminar4.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class LegoDetail {
+protected:
+	string color;
+	string shape ;
+	int width, length;
+
+public:
+	bool state = 0;
+
+
+	LegoDetail(string shape, string color, int width, int length) {
+		this->shape = shape;
+		this->color = color;
+		this->width = width;
+		this->length = length;
+	}
+
+
+	void setColor(string color)  {
+		this->color = color;
+	}
+
+	string getColor()  {
+		return color;
+	}
+	
+	void virtual toHurt() = 0;
+};
+
+class Brick : public LegoDetail {
+	
+	using LegoDetail::LegoDetail;
+public:	
+	void toHurt() override 
+	{
+		if (width == 1 && length == 1)
+			cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		
+		else  cout << "AAA";
+	}
+};
+
+//методы для условного интерфейса
+void connect(int,int,vector<Brick>&);
+void unconnect(int,int,vector<Brick>&);
+
+int main()
+{
+	vector <Brick> v1 = { {"plate","red",1,1} , {"brick","black",2,2} };
+
+	connect(0, 1, v1);
+	cout << v1[0].state << endl;
+	unconnect(0, 1, v1);
+	cout << v1[0].state << endl;
+	v1[0].toHurt();
+
+}
+
+void connect(int first, int second, vector<Brick>& v1) {
+	v1[first].state = 1;
+	v1[second].state = 1;
+}
+
+void unconnect(int first, int second, vector<Brick>& v1) {
+	v1[first].state = 0;
+	v1[second].state = 0;
+}
---
+++ Seminar5/Seminar5/Seminar5.cpp
@@ -0,0 +1,64 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    //объявление переменных и открытие файла
+    string line;
+    int counter = 0;
+    int amountOfarithmeticOperations = 0;
+    ifstream in("C:\\Users\\nikit\\source\\repos\\Seminar5\\source.txt");
+    vector <int> v;
+    vector <char> v1;
+    
+    //отделение знаков от чисел по разым векторам
+    for (in >> line; !in.eof(); in >> line) {
+        if (line[0] >= 48 && line[0] <=57){
+                counter++;
+                v.push_back(stoi(line));
+        }
+        else {
+            amountOfarithmeticOperations++;
+            v1.push_back(line[0]);
+        }
+        
+    }
+
+    //определение длинны матрицы
+    int lengthOfMatrix = ceil(counter / (amountOfarithmeticOperations + 1));
+
+    //вычисления
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] == '+') {
+            for (int j = 0; j < lengthOfMatrix; j++) {
+                v[j] += v[((i+1) * lengthOfMatrix) + j];
+            }
+        }
+
+        if (v1[i] == '-') {
+            for (int j = 0; j < lengthOfMatrix; j++) {
+                v[j] -= v[((i+1) * lengthOfMatrix) + j];
+            }
+        }
+    }
+
+    //вывод
+    for (int i = 0; i < lengthOfMatrix; i++) {
+        cout << v[i] << " ";
+        if (((i+1) % (int)sqrt(lengthOfMatrix)) == 0) {
+            cout << endl;
+        }
+    }
+
+    cout << 12 + 4 * 3 + ;
+
+
+    in.close();
+}
+
+
